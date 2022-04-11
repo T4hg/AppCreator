aba92ca9e1e5dcd406145892e58d7cca5de23382
Tahg <103221002+T4hg@users.noreply.github.com>
2022-04-11 11:41:27 +0200

    Install

---
+++ requirements.py
@@ -0,0 +1,25 @@
+import os
+try:
+    os.system("pip install colorama")
+except:
+    pass
+try:
+    os.system("pip install PyQt5")
+except:
+    pass
+try:
+    os.system("pip install requests")
+except:
+    pass
+try:
+    os.system("pip install datetime")
+except:
+    pass
+try:
+    os.system("pip install tk")
+except:
+    pass
+try:
+    os.system("pip install ctypes")
+except:
+    pass
